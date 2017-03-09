eda5de7913fdc67f93e9a33582db92f359107d34
wzulfikar-mba <wzulfikar@ymail.com>
2017-03-09 21:22:50 +0800

    add param to specify number of git log

--- bash/git-aliases.sh
+++ bash/git-aliases.sh
@@ -1,7 +1,8 @@
-## git aliases
+# aliases for common git operation
 alias commit="git commit -m '$1'"
 alias add="git add"
 alias pull="git pull"
 alias push="git push"
-alias gitlog="git log --one-line --graph"
+alias status="git status"
 alias checkpoint="git add . && git commit -m"
+alias gitlog="git log --one-line --graph -n 10"
