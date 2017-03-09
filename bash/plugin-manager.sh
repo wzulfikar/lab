02dd4baea581558b7fc63d46eabfc4504512d3cd
wzulfikar-mba <wzulfikar@ymail.com>
2017-03-09 21:23:11 +0800

    added plugin manager to centralize shell’s plugins

---
+++ bash/plugin-manager.sh
@@ -0,0 +1,11 @@
+# Put this code `source path_to_this/plugins.sh`
+# into your profile file (.bashrc, .zshrc, etc)
+# and your plugins are now centralized here.
+source ~/code/lab/bash/ocr.sh
+source ~/code/lab/bash/code.sh
+source ~/code/lab/bash/git-aliases.sh
+source ~/code/lab/bash/app-aliases.sh
+source ~/code/lab/bash/brew-switch.sh
+source ~/code/lab/bash/watch-artisan.sh
+source ~/code/lab/bash/watch-phpunit.sh
+source ~/code/lab/bash/watch-sh.sh
