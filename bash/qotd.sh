4a6dc2e4288780aac9356377f9b0077baa4a93cd
wzulfikar_mba <me@wzulfikar.com>
2017-03-11 20:02:36 +0800

    added qotd (quote of the day)

---
+++ bash/qotd.sh
@@ -0,0 +1,13 @@
+qotd () {
+	# replace this path to your own
+	d=~/code/lab/bash/qotd-words.txt
+
+	rand=$[($RANDOM % `wc -l $d|sed "s/[^0-9]//g"`)+1]
+	
+	# `printf` ansi escape code for green 
+	# so the qotd will be printed in green color.
+	# for other escape code, see https://en.wikipedia.org/wiki/ANSI_escape_code
+	printf '\033[0;32m'
+	sed $rand"q;d" $d
+}
+eval "qotd"
