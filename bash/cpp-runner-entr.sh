994c5a9912059474acfa5fd0d6e116c8f80ead70
wzulfikar_mba <me@wzulfikar.com>
2017-03-30 18:19:29 +0800

    added entr version of cpp-runner

---
+++ bash/cpp-runner-entr.sh
@@ -0,0 +1,23 @@
+run-cpp () {
+    FILENAME=$1
+    eval "g++ -o $FILENAME $FILENAME.cpp && ./$FILENAME"
+}
+
+# `entr` requires unix `entr` utility
+# → http://entrproject.org
+watch-cpp () {
+	NC='\033[0;0m' # no color
+	GREEN='\033[0;32m'
+	ORANGE='\033[0;33m'
+
+    FILENAME=$1
+
+    MSG='$ORANGE\n\n→ Watching cpp files (using entr)..$NC'
+    BANNER='${GREEN}Output of $FILENAME:$NC'
+
+    COMMAND="clear && g++ -o $FILENAME $FILENAME.cpp && echo '$BANNER' && ./$FILENAME"
+
+	FILES="find . -name \*.cpp"
+
+	eval "$FILES | entr sh -c \"$COMMAND; printf '$MSG';\""
+}
