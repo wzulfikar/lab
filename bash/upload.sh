43eb958c20537ebf45c96696a899d536d903870d
wzulfikar_mba <me@wzulfikar.com>
2017-04-09 19:28:54 +0800

    added support for optional arg, will return link to virustotal scan result.

--- bash/upload.sh
+++ bash/upload.sh
@@ -3,7 +3,17 @@
 upload () {
     # write to output to tmpfile because of progress bar
     tmpfile=$( mktemp -t transferXXX )
-    curl --progress-bar --upload-file $1 https://transfer.sh/$(basename $1) >> $tmpfile;
+    filename=$(basename $1)
+
+    # support optional arg, will return link to virustotal scan result.
+    # eg. upload malicious-file.exe virustotal
+    optional_arg=""
+    if [ $# -eq 2 ]
+      then
+        optional_arg="/$2"
+    fi
+
+    curl --progress-bar --upload-file $1 https://transfer.sh/$filename$optional_arg >> $tmpfile;
     cat $tmpfile;
     rm -f $tmpfile;
 }
