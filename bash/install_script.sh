7b859699d34ac9dc067815db76aca38dab48b9f5
wzulfikar_mba <me@wzulfikar.com>
2017-04-17 06:49:48 +0800

    added install_script.sh (WIP)

---
+++ bash/install_script.sh
@@ -0,0 +1,44 @@
+# snippets to install stuffs.
+# Note:
+# all snippets listed here have been 
+# used in an OSX environment.
+
+# google drive cli
+# https://github.com/prasmussen/gdrive
+brew install gdrive
+
+# command-line client for wunderlist
+# https://github.com/wayneashleyberry/wunderline
+npm install -g wunderline
+
+# Fast and dependency-free cryptographic hashing library (js)
+# https://github.com/h2non/jshashes
+npm i -g jshashes
+
+npm i -g surge
+
+npm install -g jira-cli
+
+brew install gifgen
+
+brew install asciinema
+
+brew install entr
+
+brew install openssl
+
+brew install ngrok
+
+brew install tesseract --all-languages
+
+composer require global laravel/valet
+
+composer global require friendsofphp/php-cs-fixer
+
+# GitHub command line tools made with NodeJS.
+# https://github.com/node-gh/gh
+npm install -g gh
+
+# Hacker News command line interface (CLI).
+# https://github.com/donnemartin/haxor-news
+pip install haxor-news
