7baaf76019637689decc348417427336ee19613d
wzulfikar_mba <me@wzulfikar.com>
2018-02-23 13:36:01 +0800

    added sample reflect code

---
+++ go/reflect.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	person := struct {
+		Name    string `json:"name"`
+		Age     int
+		married bool
+	}{
+		"John Doe",
+		34,
+		false,
+	}
+
+	rv := reflect.ValueOf(person)
+	for i := 0; i < rv.NumField(); i++ {
+		fld := rv.Field(i)
+		typeFld := rv.Type().Field(i)
+		fmt.Printf("Field #%d: %s\n", i, typeFld.Name)
+		fmt.Printf("Type    : %s\n", fld.Type())
+		fmt.Printf("Tag     : %s\n", typeFld.Tag)
+
+		fieldUnexported := typeFld.Name[0:1] == strings.ToLower(typeFld.Name[0:1])
+		if fieldUnexported {
+			fmt.Printf("Value   : not available (unexported)\n")
+		} else {
+			fmt.Printf("Value   : %v\n", fld.Interface())
+		}
+
+		fmt.Println()
+	}
+}
