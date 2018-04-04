a09418ed6b00d34a4606af662bb573257b67b5f6
wzulfikar_mba <me@wzulfikar.com>
2018-04-04 14:09:44 +0800

    Add ability to accept page range

--- go/imagescraper/cmd/main.go
+++ go/imagescraper/cmd/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/wzulfikar/lab/go/imagescraper"
 )
@@ -23,5 +27,69 @@ func main() {
 	selector := os.Args[2]
 	dir := os.Args[3]
 
+	defer fmt.Println("Done ✔")
+
+	if newUrl, from, to, pageOk := getUrlPage(url); pageOk {
+		var wg sync.WaitGroup
+		for i := from; i <= to; i++ {
+			wg.Add(1)
+			go func(i int) {
+				targetUrl := newUrl + strconv.Itoa(i)
+				fmt.Printf("[START] %s\n", targetUrl)
+
+				imagescraper.Scrape(targetUrl, selector, dir)
+				fmt.Println("[DONE] %d\n", url)
+				wg.Done()
+			}(i)
+		}
+		wg.Wait()
+		return
+	}
+
+	fmt.Println("Scraping images from", url)
 	imagescraper.Scrape(url, selector, dir)
+}
+
+func getUrlPage(url string) (string, int, int, bool) {
+	param := strBetween(url, "[", "]")
+	if param == "" || !strings.Contains(param, "-") {
+		return "", 0, 0, false
+	}
+
+	page := strings.Split(param, "-")
+	if len(page) != 2 {
+		return "", 0, 0, false
+	}
+	page1, err := strconv.Atoi(page[0])
+	if err != nil {
+		return "", 0, 0, false
+	}
+	page2, err := strconv.Atoi(page[1])
+	if err != nil {
+		return "", 0, 0, false
+	}
+
+	if page1 >= page2 {
+		log.Fatal("ERROR: URL contains invalid page. `page1` must be smaller than `page2`.")
+	}
+
+	url = strings.Replace(url, "["+param+"]", "", -1)
+	return url, page1, page2, true
+}
+
+func strBetween(str string, a string, b string) string {
+	// Get substring between two strings.
+	posFirst := strings.Index(str, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(str, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return str[posFirstAdjusted:posLast]
 }
