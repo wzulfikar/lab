9cd372753bb6b219f96c5a6ff6e96d3f78136835
wzulfikar_mba <me@wzulfikar.com>
2018-04-05 18:55:37 +0800

    Pass walker dir and number of concurrency as args. Added helper to delete file

--- go/walker.go
+++ go/walker.go
@@ -1,3 +1,4 @@
+// walk dir recursively and run command
 package main
 
 import (
@@ -6,15 +7,38 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
 
-// walk dir recursively and run command
-func main() {
-	dir := os.Getenv("WALKER_DIR")
+// adjust your code here
+func handle(path string) {
+	cmd := "python3"
+	args := []string{
+		"/Volumes/data/playground/face-postgre/face-add.py",
+		path,
+	}
+	execCmd(cmd, args)
 
-	const concurrency = 10 // change to 0 to run without concurrency
+	filename := filepath.Base(path)
+	mv(path, "/data/processed-images/"+filename)
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("USAGE : walker [dir] [concurrency]")
+		fmt.Println("SAMPLE: walker ~/data/images 10")
+		return
+	}
+	dir := os.Args[1]
+	concurrency, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("[INIT] Walking directory", dir, "with", concurrency, "concurrency")
+
 	sem := make(chan bool, concurrency)
 
 	fileCount := 0
@@ -34,7 +58,7 @@ func main() {
 			log.Printf("Processing file #%d: %s\n", fileCount, f.Name())
 
 			if f.IsDir() || f.Name() == ".DS_Store" {
-				log.Println("[SKIP]", f.Name())
+				log.Println("[SKIP]", path)
 			} else if concurrency == 0 {
 				handle(path)
 			} else if concurrency > 0 {
@@ -55,16 +79,6 @@ func main() {
 	}
 }
 
-// adjust your code here
-func handle(path string) {
-	cmd := "python3"
-	args := []string{
-		"/Volumes/data/playground/face-postgre/face-add.py",
-		path,
-	}
-	execCmd(cmd, args)
-}
-
 func execCmd(cmd string, args []string) {
 	cmdString := cmd + " " + strings.Join(args, " ")
 	log.Println("[START]", cmdString)
@@ -79,6 +93,15 @@ func execCmd(cmd string, args []string) {
 func mv(from, to string) {
 	if err := os.Rename(from, to); err != nil {
 		log.Println("mv failed:", err)
+		return
 	}
-	fmt.Println("Moved ", from, "to", to)
+	fmt.Println("[MOVE]", from, "to", to)
+}
+
+func rm(path string) {
+	if err := os.Remove(path); err != nil {
+		log.Println("rm failed:", err)
+		return
+	}
+	fmt.Println("[REMOVE]", path)
 }
