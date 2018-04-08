9941ea68a9476d7c5b7b6409f1e87a2e14578aab
wzulfikar_mba <me@wzulfikar.com>
2018-04-08 14:12:34 +0800

    Add prompt for profile id

--- python/facerec/faceadd.py
+++ python/facerec/faceadd.py
@@ -7,11 +7,28 @@ import postgresql
 import time
 
 if len(sys.argv) < 2:
-    print("Usage: face-add <image>")
+    print("Usage: face-add <image> <profile_id (optional)>")
     exit(1)
 
+PREVIEW_WINDOW = 'FaceAdd Preview'
+cv2.namedWindow(PREVIEW_WINDOW, cv2.WINDOW_NORMAL)
 
-def faceadd(db, face_detector, file_name, profile_id=None):
+
+def preview(windowname, img, label):
+    preview = img
+    cv2.putText(preview,
+                label,
+                (10, 25),
+                cv2.FONT_HERSHEY_DUPLEX,
+                0.5,
+                (0, 0, 255),
+                1)
+    cv2.imshow(windowname, preview)
+    cv2.waitKey(1)
+
+
+def faceadd(db, face_detector, file_name, to_profile_id=None):
+    promptprofile = False
 
     # Load the image
     image = cv2.imread(file_name)
@@ -31,10 +48,12 @@ def faceadd(db, face_detector, file_name, profile_id=None):
 
     # Loop through each face we found in the image
     for i, face_rect in enumerate(detected_faces):
+        profile_id = to_profile_id
+
         # Detected faces are returned as an object with the coordinates
         # of the top, left, right and bottom edges
-        print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i + 1, face_rect.left(), face_rect.top(),
-                                                                                 face_rect.right(), face_rect.bottom()))
+        print("- Face #{} found: at Left: {} Top: {} Right: {} Bottom: {}".format(i + 1, face_rect.left(), face_rect.top(),
+                                                                                  face_rect.right(), face_rect.bottom()))
         crop = image[face_rect.top():face_rect.bottom(),
                      face_rect.left():face_rect.right()]
         encodings = face_recognition.face_encodings(crop)
@@ -42,7 +61,18 @@ def faceadd(db, face_detector, file_name, profile_id=None):
         if not len(encodings):
             print("  [SKIP] face has no encodings")
         else:
-            print("- Adding face for profile <{}>".format(profile_id))
+            if not profile_id:
+                insertprofile = None
+                while(insertprofile is None):
+                    previewimg = image[face_rect.top() - 25:face_rect.bottom() + 25,
+                                       face_rect.left() - 25:face_rect.right() + 25]
+                    preview(PREVIEW_WINDOW, previewimg, 'F.{}'.format(i + 1))
+                    insertprofile = input(
+                        "> Insert profile ID for face #{} (press enter to skip): ".format(i + 1))
+                    if insertprofile != "":
+                        profile_id = insertprofile
+
+            print("- Adding face #{} to profile <{}>".format(i + 1, profile_id))
             if not profile_id:
                 query = "INSERT INTO vectors (file, vec_low, vec_high) VALUES ('{}', CUBE(array[{}]), CUBE(array[{}]))".format(
                     file_name,
@@ -102,6 +132,8 @@ else:
             if faceadd(db, face_detector, filepath, profile_id) is not False:
                 filecount += 1
                 print("[DONE] File #{} added: {}".format(filecount, filepath))
+
+cv2.destroyAllWindows()
 
 print("Files processed:", filecount)
 print("Time elapsed:", time.time() - start)
