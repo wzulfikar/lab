ba0049c21aaf77d1f9d1b659e6f23b5b66ce47b4
wzulfikar_mba <me@wzulfikar.com>
2018-04-10 19:46:39 +0800

    adjust watch-py for python2 and python3

--- bash/watch-py-entr.sh
+++ bash/watch-py-entr.sh
@@ -1,6 +1,6 @@
 # `entr` requires unix `entr` utility
 # → http://entrproject.org
-watch-py () {
+watch-py2 () {
 	NC='\033[0;0m' # no color
 	GREEN='\033[0;32m'
 
@@ -9,7 +9,23 @@ watch-py () {
     MSG='\n→ Watching python (using entr)..$NC'
     BANNER='${GREEN}Output of $FILENAME:$NC'
 
-    COMMAND="clear && echo '$BANNER' && python $FILENAME "
+    COMMAND="clear && echo '$BANNER' && python3 $FILENAME "
+
+	FILES="find . -name \*.py"
+
+	eval "$FILES | entr sh -c \"$COMMAND; printf '$MSG';\""
+}
+
+watch-py3 () {
+	NC='\033[0;0m' # no color
+	GREEN='\033[0;32m'
+
+    FILENAME=$1
+
+    MSG='\n→ Watching python (using entr)..$NC'
+    BANNER='${GREEN}Output of $FILENAME:$NC'
+
+    COMMAND="clear && echo '$BANNER' && python3 $FILENAME "
 
 	FILES="find . -name \*.py"
 
