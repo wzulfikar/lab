3dd27352daf7168f0f26dcf904f6b8036eb2d902
Wzulfikar <me@wzulfikar.com>
2019-06-27 03:12:59 +0800

    hugo: add script to publish updated blog.

---
+++ hugo/publish.sh
@@ -0,0 +1,17 @@
+# initiate dist directory if not exist
+if [ ! -d dist ]; then
+	git clone https://github.com/wzulfikar/wzulfikar.com.git dist
+else
+	git pull origin master
+fi
+
+# generate static files
+hugo --minify
+
+# push static files to remote repo
+(
+	cd dist
+	git add .
+	git commit -m 'build'
+	git push -u origin master
+)
