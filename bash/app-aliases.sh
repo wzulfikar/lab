1bcb5c5c0dcfb7801a9849a2c093737287a14074
wzulfikar <me@wzulfikar.com>
2019-09-15 21:20:45 +0800

    chore: add new scripts

--- bash/app-aliases.sh
+++ bash/app-aliases.sh
@@ -3,3 +3,12 @@ alias macdown="/Applications/MacDown.app/Contents/MacOS/MacDown"
 alias md="macdown"
 alias serve="php -S 0.0.0.0:8888"
 alias st="open -a SourceTree"
+
+# installation: go get -u github.com/jesseduffield/lazygit
+alias lgit="lazygit"
+
+# installation: go get -u github.com/jesseduffield/lazydocker
+alias ldocker="lazydocker"
+
+# installation: go get -u github.com/martinlindhe/imgcat
+alias catimg="imgcat"
