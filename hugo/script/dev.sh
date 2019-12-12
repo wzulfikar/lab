9ca167c5c370ed80c9831cb4c75a97f6f5b70b17
wzulfikar <me@wzulfikar.com>
2019-12-12 10:49:17 +0800

    chore(hugo): change serve.sh to dev.sh

--- hugo/script/dev.sh
+++ hugo/script/dev.sh
@@ -3,6 +3,8 @@
 # make env vars accessible by hugo
 export $(cat .env)
 
+echo "[INFO] service hugo for dev"
+
 HUGO_LOCAL_DEV=true \
 BUILD_ID=$(git describe --always) \
 hugo server -F -D --config ./config.dev.toml
