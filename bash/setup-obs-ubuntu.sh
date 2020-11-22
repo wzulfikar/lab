ff817dbeb2a8a3f063756f0add2c1b9df7740fdc
wzulfikar <me@wzulfikar.com>
2020-11-22 19:08:40 +0200

    fix(setup-obs-ubuntu.sh): fix wrong order for vnc server custom port config

--- bash/setup-obs-ubuntu.sh
+++ bash/setup-obs-ubuntu.sh
@@ -23,12 +23,9 @@ apt update -y &&
     /opt/VirtualGL/bin/vglserver_config -config -s -f -t &&
     wget -O turbovnc_2.2.5_amd64.deb https://sourceforge.net/projects/turbovnc/files/2.2.5/turbovnc_2.2.5_amd64.deb/download &&
     dpkg -i turbovnc_*.deb &&
+    sed 's/$vncPort = 5900 + $displayNumber;/$vncPort = 5900 + $displayNumber;\nif(defined $ENV{'BASE_PORT'}) {\n  $vncPort = $ENV{'BASE_PORT'} + $displayNumber;\n}/' /opt/TurboVNC/bin/vncserver &&
     BASE_PORT=6700 /opt/TurboVNC/bin/vncserver &&
     /usr/bin/screen -dmS obs /usr/bin/sh -c "DISPLAY=:1 /usr/bin/obs --studio-mode" &&
-    sed 's/$vncPort = 5900 + $displayNumber;/$vncPort = 5900 + $displayNumber;\nif(defined $ENV{'BASE_PORT'}) {\n  $vncPort = $ENV{'BASE_PORT'} + $displayNumber;\n}/' /opt/TurboVNC/bin/vncserver &&
     echo '@reboot BASE_PORT=6700 /opt/TurboVNC/bin/vncserver' >/tmp/crontab-obs &&
     echo '@reboot /usr/bin/screen -dmS obs /usr/bin/sh -c "DISPLAY=:1 /usr/bin/obs --studio-mode"' >>/tmp/crontab-obs &&
-    (
-        crontab -l
-        cat /tmp/crontab-obs
-    ) | crontab -
+    (crontab -l ; cat /tmp/crontab-obs )| crontab -
