7bf862e3d55a63dd81a5b359c656c5847e5094d7
wzulfikar <hey@wzulfikar.com>
2021-04-30 07:41:45 +0700

    Use variable for repo directory

--- bash/plugin-manager.sh
+++ bash/plugin-manager.sh
@@ -1,23 +1,26 @@
 # Put this code `source path_to_this/plugins.sh`
 # into your profile file (.bashrc, .zshrc, etc)
 # and your plugins are now centralized here.
-source ~/code/github/lab/bash/misc.sh
-source ~/code/github/lab/bash/cpp-runner-entr.sh
-source ~/code/github/lab/bash/git-aliases.sh
-source ~/code/github/lab/bash/app-aliases.sh
-source ~/code/github/lab/bash/brew-switch.sh
-source ~/code/github/lab/bash/watch-artisan-entr.sh
-source ~/code/github/lab/bash/watch-phpunit-entr.sh
-source ~/code/github/lab/bash/watch-java-entr.sh
-source ~/code/github/lab/bash/docker-snippets.sh
-source ~/code/github/lab/bash/openssl-enc-dec.sh
-# source ~/code/github/lab/bash/bitly.sh
-source ~/code/github/lab/bash/watch-py-entr.sh
-source ~/code/github/lab/bash/watch-js-entr.sh
-source ~/code/github/lab/bash/watch-php-entr.sh
-# source ~/code/github/lab/bash/watch-asm-entr.sh
-source ~/code/github/lab/bash/getlinks.sh
-# source ~/code/github/lab/bash/run-asm.sh
-# source ~/code/github/lab/bash/gdrive-file.sh
-source ~/code/github/lab/bash/bash-runner.sh
-source ~/code/github/lab/bash/proxify.sh
+
+REPO_DIRECTORY=~/code/github/lab
+
+source $REPO_DIRECTORY/bash/misc.sh
+source $REPO_DIRECTORY/bash/cpp-runner-entr.sh
+source $REPO_DIRECTORY/bash/git-aliases.sh
+source $REPO_DIRECTORY/bash/app-aliases.sh
+source $REPO_DIRECTORY/bash/brew-switch.sh
+source $REPO_DIRECTORY/bash/watch-artisan-entr.sh
+source $REPO_DIRECTORY/bash/watch-phpunit-entr.sh
+source $REPO_DIRECTORY/bash/watch-java-entr.sh
+source $REPO_DIRECTORY/bash/docker-snippets.sh
+source $REPO_DIRECTORY/bash/openssl-enc-dec.sh
+source $REPO_DIRECTORY/bash/bitly.sh
+source $REPO_DIRECTORY/bash/watch-py-entr.sh
+source $REPO_DIRECTORY/bash/watch-js-entr.sh
+source $REPO_DIRECTORY/bash/watch-php-entr.sh
+source $REPO_DIRECTORY/bash/watch-asm-entr.sh
+source $REPO_DIRECTORY/bash/getlinks.sh
+source $REPO_DIRECTORY/bash/run-asm.sh
+source $REPO_DIRECTORY/bash/gdrive-file.sh
+source $REPO_DIRECTORY/bash/bash-runner.sh
+source $REPO_DIRECTORY/bash/proxify.sh
