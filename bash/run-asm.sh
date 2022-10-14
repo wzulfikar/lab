2ef21c16150a88a15bfb7bf4ef0014fbee262ed3
Wildan Zulfikar <hey@wzulfikar.com>
2022-10-14 23:31:15 +0300

    Move bash scripts to wzulfikar/scripts

--- bash/run-asm.sh
+++
@@ -1,5 +0,0 @@
-run-asm () {
-    FILENAME=$1
-    OUTPUT_FILE=asm_tmp.o
-    nasm -f macho64 -o $OUTPUT_FILE $1 && ld $OUTPUT_FILE -o $OUTPUT_FILE && ./$OUTPUT_FILE
-}
--- bash/snippets.sh
+++
@@ -1,6 +0,0 @@
-# move user's folder to external directory (OSX).
-NEW_DIR=/Volumes/data
-ln -s $NEW_DIR/Downloads ~
-ln -s $NEW_DIR/Pictures ~
-ln -s $NEW_DIR/Music ~
-ln -s $NEW_DIR/Archive/dump_desktop ~/Desktop
